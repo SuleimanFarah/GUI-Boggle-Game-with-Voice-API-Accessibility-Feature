5b16f27ddf7427e74d26a90a4ce0fb1aa76bdd54
christophermarcow <117307018+christophermarcow@users.noreply.github.com>
2022-12-06 22:52:25 -0500

    Added some documentation for BoggleMusic
    Give brief explanation in the BoggleMusic class.

--- Assignment1Default/boggle/BoggleMusic.java
+++ Assignment1Default/boggle/BoggleMusic.java
@@ -9,16 +9,19 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class BoggleMusic {
     private Clip sound;
+    //function to open up the file and start the music.
     public void start(String filename) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         Clip clip = AudioSystem.getClip();
         sound = clip;
         clip.open(AudioSystem.getAudioInputStream(new File(filename)));
         clip.start();
     }
+    //function to play the music after a pause. Music will loop continuosly until the app is closed by the user.
     public void play(){
         sound.start();
         sound.loop(Clip.LOOP_CONTINUOUSLY);
     }
+    //function to pause the music.
     public void pause(){
         sound.stop();
     }
